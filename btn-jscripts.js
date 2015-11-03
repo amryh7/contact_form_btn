c87a027cd414afa5055b058effacdadcd49a2a0f
Anthony Roy <amryh7@gmail.com>
2015-11-03 03:13:51 -0600

    init commit

---
+++ btn-jscripts.js
@@ -0,0 +1,33 @@
+$().ready(function(){
+	
+	//alert("linked");
+	$('.btn-box img').on('click', function(){});
+	
+	
+	// button 2 click function
+	$('.div-btn').on('click', function(){
+		$this = $(this);
+		//alert($(this).css("background-color"));
+		if($(this).css("background-color") == "rgb(87, 156, 1)"){
+			$(this).css({
+						"background":"rgb(154, 156, 157) url('ex-2.png') no-repeat center center",
+						"background-size":"60%",
+						"-ms-transform":"rotate(45deg)", /* IE 9 */
+						"-webkit-transform":"rotate(45deg)", /* Chrome, Safari, Opera */
+						"transform":"rotate(45deg)",
+						"border-top-left-radius":"0",
+			});
+			$('.ex-form').css({"display":"block"});
+		} else {
+			$(this).css({
+						"background":"rgb(87, 156, 1) url('ex-1.png') no-repeat center center",
+						"background-size":"60%",
+						"-ms-transform":"rotate(-25deg)", /* IE 9 */
+						"-webkit-transform":"rotate(-25deg)", /* Chrome, Safari, Opera */
+						"transform":"rotate(-25deg)",
+						"border-top-left-radius":"50%",
+					});
+			$('.ex-form').css({"display":"none"});
+		}
+	});
+});
