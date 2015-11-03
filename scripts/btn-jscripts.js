51d6da1586c68b2f01903be5f0cc50356423396b
Anthony Roy <amryh7@gmail.com>
2015-11-03 03:25:56 -0600

    file abstraction

--- scripts/btn-jscripts.js
+++ scripts/btn-jscripts.js
@@ -10,7 +10,7 @@ $().ready(function(){
 		//alert($(this).css("background-color"));
 		if($(this).css("background-color") == "rgb(87, 156, 1)"){
 			$(this).css({
-						"background":"rgb(154, 156, 157) url('ex-2.png') no-repeat center center",
+						"background":"rgb(154, 156, 157) url('../assets/ex-2.png') no-repeat center center",
 						"background-size":"60%",
 						"-ms-transform":"rotate(45deg)", /* IE 9 */
 						"-webkit-transform":"rotate(45deg)", /* Chrome, Safari, Opera */
@@ -20,7 +20,7 @@ $().ready(function(){
 			$('.ex-form').css({"display":"block"});
 		} else {
 			$(this).css({
-						"background":"rgb(87, 156, 1) url('ex-1.png') no-repeat center center",
+						"background":"rgb(87, 156, 1) url('../assets/ex-1.png') no-repeat center center",
 						"background-size":"60%",
 						"-ms-transform":"rotate(-25deg)", /* IE 9 */
 						"-webkit-transform":"rotate(-25deg)", /* Chrome, Safari, Opera */
